a02da8c37c4b13a1477c076335f0ca52990841eb
周宇琛 <yuchenzhou2001@163.com>
2024-04-05 21:22:28 +0800

    v1.1 更新了Docker部署

--- Station/settings.py
+++ Station/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     # 添加中间件 LocalMiddleware
     'django.middleware.locale.LocaleMiddleware', #国际化和本地化功能
     'django.middleware.common.CommonMiddleware', #处理请求信息，规范化请求内容
-    'django.middleware.csrf.CsrfViewMiddleware', #开启CSRF防护功能
+    # 'django.middleware.csrf.CsrfViewMiddleware', #开启CSRF防护功能
     'django.contrib.auth.middleware.AuthenticationMiddleware', #内置的用户认证系统
     'django.contrib.messages.middleware.MessageMiddleware', #内置的信息提示功能
     'django.middleware.clickjacking.XFrameOptionsMiddleware', #防止恶意程序单机劫持
--- tools.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-def convert_avi_to_mp4(input_path, output_path):
-    clip = VideoFileClip(input_path)
-    clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
-
-convert_avi_to_mp4('back/static/video/track.avi', 'back/static/video/track.mp4')
